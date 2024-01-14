33e4dda44c004a6d18d128d02c98d01b88994e6a
Danil <evseenkov143@gmail.com>
2024-01-14 20:17:49 +0600

    sada

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -30,7 +30,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Автомобили</title>
-    <link rel="stylesheet" href="/css/catalog.css">
+    <link rel="stylesheet" href="/css/show.css">
     <link rel="icon" href="/favicon.ico" type="image/x-icon">
 </head>
 <body>
